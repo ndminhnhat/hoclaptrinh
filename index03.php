6a95724c8cae7945e24c458f321d9005e25d7276
ndminhnhat <52247607+ndminhnhat@users.noreply.github.com>
2019-06-30 15:11:33 +0700

    Add files via upload

---
+++ index03.php
@@ -0,0 +1,59 @@
+<?php
+$a = 600;
+$b = 962;
+$c = 396;
+//if ($a>$b && $a>$c) echo 'max la ' .$a;
+ //else if ($b>$c) echo 'max la ' .$b;
+
+  //else echo ' Max la ' .$c;
+ //echo ' max la '. ((($a >=$b) && ($a>=$c)) ? $a : ($b >= $c ? $b : $c) );
+ //echo  ' max la  ' .(($a >=$b ? $a : $b) >= $c ? ($a >=$b ? $a : $b) : $c);
+ //
+
+ <?php
+ // $a = 'Hello';
+ // $b = 'Teo';
+ // echo $a.' '.$b;
+ // echo $a .= ' '.$b; 
+ // $a = 7;
+ // $b = 3;
+ // echo $a %= $b; // 1
+ // echo $a+$b;
+ // echo $a+=$b; // 4
+ // echo $a; // 4
+ // echo $b; // 3
+ // echo $a + 1; 
+ // echo $a++; // $a = $a + 1;
+ // echo ++$a; // 3
+ // echo $a; // 3
+ // echo $a += 1; //$a = $a + 1;
+ // echo --$a;
+ // echo $a -= 3; //$a = 1-3 = -2
+ // $a = '';  echo $a; 
+ // $b = null; echo $b;
+ // $a = 1;
+ // $b = '1';
+ // $check = true;
+ // echo !$check; 
+ // if(!(1==2)){
+ //     echo 'Dung';
+ // }
+ // else echo 'Sai';
+ $a = 120;
+ $b = 0;
+ $c = 10;
+ echo ($a >= $b ? $a : $b) >= $c ? ($a >= $b ? $a : $b) : $c;
+ //          $m             >= $c ?  $m : $c
+ echo ( (($a >= $b) && ($a >= $c)) ? $a : ($b >= $c ? $b : $c) );
+ // if($a>=$b) echo $a;
+ // else echo $b; 
+ // echo 'max la '. ($a <= $b ? $b : $a);  
+ // dieukien ? giatri_dieukien_dung : giatri_dieukien_sai;
+ // $c = 30;
+ // if ($a>=$b && $a>=$c) echo 'max la '.$a;
+ // else if ($b>=$c) echo 'max la '.$b;
+ // else echo 'max la '.$c;
+ ?>
+
+
+?>
