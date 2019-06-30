711164a14e580828da2505d716dc3636b47c4056
ndminhnhat <52247607+ndminhnhat@users.noreply.github.com>
2019-06-30 15:06:59 +0700

    Add files via upload

---
+++ index05.php
@@ -0,0 +1,49 @@
+<?php
+// for($i = 1; $i<=10; $i++){
+//     echo "$i ";
+// }
+// echo $i;
+// $i = 1;
+// while($i<=10){
+//     echo "$i ";
+//     $i++;
+// }
+// $i = 1;
+// do {
+//     if($i%2 != 0){
+//         echo "$i ";
+//     }
+//     $i++;
+// } while ($i <= 10);
+// for($i=2; $i<=10; $i++ ){
+//     if($i%2==0) echo "$i ";
+// }
+//$arr = [2, 4, 5, 4, 8, 11, 12];
+//foreach($arr as $item){
+  //  echo $item. ' ';
+
+// $count = count($arr); // 6
+// for($i=0; $i < $count; $i++){
+//     if($i % 2 == 0){
+//         echo $arr[$i]. ' ';
+//     }
+// }
+// echo $arr[0];
+// echo $arr[1];
+// echo $arr[2];
+
+for ($i=1; $i <=10 ; $i++) { 
+  $tong += $i;
+}
+echo $tong;
+
+//$tong=0;
+//$i=1;
+//while($i<=10)
+//{
+  // $i++;
+  //$tong+=$i ;
+  //}
+  //echo $tong;
+
+    ?>
